3427dc3ba45d01972eadbca7db73d0ecce89a1f3
BhavyaYash290 <91058078+BhavyaYash290@users.noreply.github.com>
2022-06-05 20:49:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+}
+function draw(){}
+function takeSnapshot(){
+    save('MyPhoto.png')
+}
